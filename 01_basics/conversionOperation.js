1983e70deca32f1a590d0bec3ffee1d93199011a
Ayush Chanchal <ayushchanchal447@gmail.com>
2024-03-20 21:55:10 +0000

    ag

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,12 +1,36 @@
-let score=33
-console.log(typeof(score));
+// let score=33
+// console.log(typeof(score));
 
-let score1="22";
-console.log(typeof(score1));
+// let score1="22";
+// console.log(typeof(score1));
 
-let score2=Number(score1);
-console.log(typeof(score2));
+// let score2=Number(score1);
+// console.log(typeof(score2));
 
-let score3="1Ayush";
-Number(score3);
-console.log(typeof score3);
+// let score3="1Ayush";
+// Number(score3);
+// console.log(typeof score3);
+
+
+
+// ----------------operations------------------------------
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+// let str1="Hello";
+// let str2=" Ayush";
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+"2");
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(true);
+console.log(+true);// 1
+// console.log(true+);//give error
